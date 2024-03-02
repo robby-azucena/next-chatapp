f1a03e56453e411a2e495734aa8a57fab89dd13f
Robby <robby.azucena@gmail.com>
2024-03-02 14:02:47 +0800

    dev

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
 import { Loader2 } from "lucide-react";
 import { ButtonHTMLAttributes, FC } from "react";
@@ -38,7 +39,11 @@ export const Button: FC<propType> = ({
   ...props
 }) => {
   return (
-    <button className="" disabled={isLoading} {...props}>
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
       {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
       {children}
     </button>
